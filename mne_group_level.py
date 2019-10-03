536008a93c55afe5c622bd369c0a718beb4f6e9f
Alex <a.irvine.10@aberdeen.ac.uk>
2019-10-03 17:48:30 +0100

    Updates to joblib code - now returns filelist hopefully - TODO flag so raw script partial matches so we also include eog and ecg absent files (at the moment it will redo them)

--- mne_group_level.py
+++ mne_group_level.py
@@ -2,11 +2,11 @@ import red_meg_tools
 import os
 
 # pointers to our directories
-rawdir = '/imaging/ai05/phono_oddball/maxfilt_raws'  # raw fifs to input
-mne_save_dir = '/imaging/ai05/phono_oddball/mne_files' # where to save MNE MEG files
-source_dir = '/imaging/ai05/phono_oddball/mne_source_models' #where to save MNE source recon files
-struct_dir = '/imaging/ai05/phono_oddball/structurals_renamed' # where our structs are
-fs_sub_dir = '/imaging/ai05/phono_oddball/fs_subdir' # fresurfer subject dir
+rawdir = '/imaging/ai05/phono_oddball/maxfilt_raws'   # raw fifs to input
+mne_save_dir = '/imaging/ai05/phono_oddball/mne_files'  # where to save MNE MEG files
+source_dir = '/imaging/ai05/phono_oddball/mne_source_models'  # where to save MNE source recon files
+struct_dir = '/imaging/ai05/phono_oddball/structurals_renamed'  # where our structs are
+fs_sub_dir = '/imaging/ai05/phono_oddball/fs_subdir'  # fresurfer subject dir
 
 
 # make a list of files for pre-processing
@@ -18,6 +18,7 @@ saved_list = red_meg_tools.preprocess_multiple(flist=flist[0:8],
                                                outdir=mne_save_dir,
                                                overwrite=False,
                                                njobs=8)
+
 # make a list of files for epoching from above
 flist = [os.path.basename(x) for x in saved_list] # get filenames only
 
--- red_meg_tools.py
+++ red_meg_tools.py
@@ -5,7 +5,7 @@ import joblib
 
 
 def preprocess_multiple(flist, indir, outdir, overwrite, njobs):
-    """
+    """ Takes a list of raw files and preprocesses them
     Parameters
     ----------
     :param flist:
@@ -36,14 +36,14 @@ def preprocess_multiple(flist, indir, outdir, overwrite, njobs):
             saved_files.append(savedfile)
     if njobs > 1:
 
-        joblib.Parallel(n_jobs =njobs)(
+        saved_files = joblib.Parallel(n_jobs =njobs)(
             joblib.delayed(__preprocess_individual)(os.path.join(indir, thisF), outdir, overwrite) for thisF in flist)
 
     return saved_files
 
 
 def __preprocess_individual(file, outdir, overwrite):
-    """
+    """ Internal function for preprocessing raw MEG files
     :param file:
         input file along with path
     :param outdir:
@@ -53,7 +53,7 @@ def __preprocess_individual(file, outdir, overwrite):
 
     """
     save_file_path = ""
-    raw = mne.io.read_raw_fif(file, preload=True)
+
     f_only = os.path.basename(file).split('_')  # get filename parts seperated by _
     num = f_only[0]
 
@@ -64,6 +64,8 @@ def __preprocess_individual(file, outdir, overwrite):
             save_file_path = f'{outdir}/{num}_{f_only[2]}_clean_raw.fif'
             return save_file_path
 
+    # read file
+    raw = mne.io.read_raw_fif(file, preload=True)
 
     # 50 Hz remove power line noise with a notch filter
     raw.notch_filter(np.arange(50, 241, 50), filter_length='auto',
@@ -81,24 +83,17 @@ def __preprocess_individual(file, outdir, overwrite):
     eog_epochs = mne.preprocessing.create_eog_epochs(raw)  # get epochs of eog (if this exists)
     eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, threshold=1)  # try and find correlated components
 
+    # define flags for tracking if we found components matching or not
+    no_ecg_removed = False
+    no_eog_removed = False
+
     # if we have identified something !
     if np.any([abs(i) >= 0.2 for i in eog_scores]):
         ica.exclude.extend(eog_inds[0:3])
 
-    # if we can't do this manually by plotting
     else:
-        # TODO: Practically this should be moved to after all autodetect is done. This way we don't waste time waiting for
-        # user input
-        ica.plot_components(inst=raw)
-        print('There is no components automatically corellated with blinks')
-        print('This is usually because the EOG electrode is bad so select components manually:')
-        man_inds = list()
-        numb = int(input("Enter how many components to get rid of:"))
-        print('Enter each index (remember 1 = 0 in Python):')
-        for i in range(int(numb)):
-            n = input("num :")
-            man_inds.append(int(n))
-        ica.exclude.extend(man_inds)
+        print(f'{num} run {f_only[2]} cannot detect eog automatically manual ICA must be done')
+        no_eog_removed = True
 
     # now we do this with hearbeat
 
@@ -111,28 +106,26 @@ def __preprocess_individual(file, outdir, overwrite):
         ica.apply(inst=raw)  # apply to raw
 
     else:  # flag for manual ICA inspection and removal
-        # TODO: Practically this should be moved to after all autodetect is done. This way we don't waste time waiting for
-        # user input
-        ica.plot_components(inst=raw)
-        print('There is no components automatically corellated with heartbeat')
-        print('This is usually because the ECG electrode is bad so select components manually:')
-        man_inds = list()
-        numb = int(input("Enter how many components to get rid of:"))
-        print('Enter each index (remember 1 = 0 in Python):')
-        for i in range(int(numb)):
-            n = input("num :")
-            man_inds.append(int(n))
-        ica.exclude.extend(man_inds)
-        ica.apply(inst=raw)
+        print(f'{num} run {f_only[2]} cannot detect ecg automatically manual ICA must be done')
+        no_ecg_removed = True
 
     # save the file
-    raw.save(f'{outdir}/{num}_{f_only[2]}_clean_raw.fif', overwrite=overwrite)
-    save_file_path = f'{outdir}/{num}_{f_only[2]}_clean_raw.fif'
+    if no_ecg_removed and no_eog_removed:
+        outfname = f'{outdir}/{num}_{f_only[2]}_noeog_noecg_clean_raw.fif'
+    elif no_ecg_removed:
+        outfname = f'{outdir}/{num}_{f_only[2]}_noecg_clean_raw.fif'
+    elif no_eog_removed:
+        outfname = f'{outdir}/{num}_{f_only[2]}_noeog_clean_raw.fif'
+    else:
+        outfname = f'{outdir}/{num}_{f_only[2]}_clean_raw.fif'
+
+    raw.save(outfname, overwrite=overwrite)
+    save_file_path = outfname
     # return
     return save_file_path
 
 def epoch_multiple(flist, indir, outdir, keys, trigchan, times, overwrite, njobs):
-    """
+    """ Epochs a list of files using the passed in arguments.
     Parameters
     ----------
     :param flist:
@@ -171,20 +164,27 @@ def epoch_multiple(flist, indir, outdir, keys, trigchan, times, overwrite, njobs
             saved_files.append(savedfile)
     if njobs > 1:
 
-        joblib.Parallel(n_jobs =njobs)(
+        saved_files = joblib.Parallel(n_jobs =njobs)(
             joblib.delayed(__epoch_individual)(os.path.join(indir, thisF), outdir, keys, trigchan, times, overwrite) for thisF in flist)
 
     return saved_files
 
 def __epoch_individual(file, outdir, keys, trigchan , times ,overwrite):
-    """
+    """ Internal function to make epochs from raw files
     :param file:
         input file along with path
     :param outdir:
         where we want to save this
     :return: save_file_path:
         a path to the saved and filtered file
-
+    :param keys:
+        dictionary pairs of labels and trigger numbers for events
+    :param trigchan:
+        the channel to look for trigger from
+    :param times:
+        list of two times to index from
+    :param overwrite:
+        truee or false. whether to overwrite the files if already exist
 
     """
     raw = mne.io.read_raw_fif(file, preload=True)
@@ -192,6 +192,7 @@ def __epoch_individual(file, outdir, keys, trigchan , times ,overwrite):
     num = f_only[0]
 
     # check if file exists, if not overwrite then skip and return path
+    # TODO: write this so it partial matches and looks for _noecg and _noeog flags in raw data
     if os.path.isfile(f'{outdir}/{num}_{f_only[2]}_clean_raw.fif'):
         if not overwrite:
             print(f'file for {num} run {f_only[2]} already exists, skipping to next')
